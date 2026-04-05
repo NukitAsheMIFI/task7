4cea0cf76d6a7f8556022659a9a5858f62d2698b
Ivan <ivan@NOTEBOOKDELLG3.localdomain>
2026-04-05 16:43:43 +0300

    добавлен вывод текста из файлов, создана функция для чтения и записи в файл

--- main.c
+++ main.c
@@ -6,19 +6,20 @@
 #include <sys/wait.h>
 #include <string.h>
 
+int read_and_write(FILE *file_joint, char *fname, char* member);
+
 int main(int argc, char **argv){
 	if (argc < 2){
 		printf("недостаточно аргументов");
+		return 1;
 	}
 	char *fname_joint = argv[1];
-	char *fname_parent = "parent_copy";
-	char *fname_child = "child_copy";
 	FILE* file_joint = fopen(fname_joint, "r"); //argv[1]
 	if (file_joint == NULL){
 		perror("can't open file for read");
 		return errno;
 	}
-	pid_t pid;
+	pid_t pid; 
 	int status;
 	switch(pid = fork()){
 		case -1:
@@ -29,41 +30,10 @@ int main(int argc, char **argv){
             printf("Child: My parent's PID -- %d\n", getppid());
             printf("Child: Enter my return code\n");
             scanf("%d", &status);
-
             printf("Child: do job\n");
-
             
-            FILE* file_child = fopen(fname_child, "w+r");
-            if (file_child == NULL){
-                perror("can't open child file");
-                fclose(file_joint);
-                return errno;
-            }
-
-			fseek(file_joint, 0, SEEK_END); 
-			size_t len = ftell(file_joint); 
-			rewind(file_joint); 
-			char *text = malloc(len + 1);
-			size_t bytes_read = fread(text, 1, len, file_joint); 
-			if (bytes_read < len) {;
-				perror("fread error");
-				fclose(file_child);
-				fclose(file_joint);
-				free(text);
-				return errno;
-			}
-
-			size_t bytes_write = fwrite(text, 1, len, file_child); 
-			if (bytes_write < len) {;
-				perror("fwrite error");
-				fclose(file_child);
-				fclose(file_joint);
-				free(text);
-				return errno;
-			}			
-
+            read_and_write(file_joint, "child_copy", "Child");
 			fclose(file_joint);
-			fclose(file_child);
 			
             printf("Child: Exit\n");
             exit(status);
@@ -79,45 +49,37 @@ int main(int argc, char **argv){
             } else {
                 printf("Parent: Child did not exit normally\n");
             }
-
 			printf("Parent: do job\n");
 
-			
-			FILE* file_parent = fopen(fname_parent, "w+r");
-			if (file_parent == NULL){
-				perror("can't open parent file");
-				fclose(file_joint);
-				return errno;
-			}
-			
-			fseek(file_joint, 0, SEEK_END); 
-			size_t len = ftell(file_joint); 
-			rewind(file_joint); 
-			char *text = malloc(len + 1);
-			size_t bytes_read = fread(text, 1, len, file_joint); 
-			if (bytes_read < len) {;
-				perror("fread error");
-				fclose(file_parent);
-				fclose(file_joint);
-				free(text);
-				return errno;
-			}
-
-			size_t bytes_write = fwrite(text, 1, len, file_parent); 
-			if (bytes_write < len) {;
-				perror("fwrite error");
-				fclose(file_parent);
-				fclose(file_joint);
-				free(text);
-				return errno;
-			}
-			
-
+			read_and_write(file_joint, "parent_copy", "Parent");
 			fclose(file_joint);			
-		    fclose(file_parent); 
 		    
             printf("Parent: Exit\n");
 		}
     }       
 	return 0;
+}
+
+int read_and_write(FILE *file_joint, char *fname, char* member){
+	FILE* file_out = fopen(fname, "w+r");
+	if (file_out == NULL){
+	    perror("can't open file for write");
+	    return errno;
+	}
+
+	rewind(file_joint);
+	char buf[1024];
+	size_t bytes;
+	while ((bytes = fread(buf, 1, sizeof(buf), file_joint)) > 0) {
+	    fwrite(buf, 1, bytes, file_out);
+	}
+
+	rewind(file_out);
+	printf("%s: ", member);
+	while ((bytes = fread(buf, 1, sizeof(buf), file_out)) > 0) {
+	    fwrite(buf, 1, bytes, stdout);
+	}
+	
+    fclose(file_out);
+    return 0;
 }
